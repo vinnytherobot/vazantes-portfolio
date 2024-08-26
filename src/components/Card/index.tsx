be68257708044bc74161740149cecd3fa3424fa0
vinnytherobot <dacostamacielf@gmail.com>
2024-08-26 11:20:52 -0300

    Update in responsive layout

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { StaticImageData } from "next/dist/shared/lib/get-img-props"
 import Image from "next/image"
 import Link from "next/link"
@@ -23,16 +25,20 @@ export default function Card({ title, description, image, link, path, imageFont 
 
                     {link && <Link href={path!} target="_blank">{link}</Link>}
                 </div>
+
+                {/* Responsive */}
                 <div className={styles.responsiveInfo}>
                     <h2 className={styles.title}>{title}</h2>
                     <h4 className={styles.description}>{description}</h4>
 
                     {link && <Link href={path!} target="_blank">{link}</Link>}
                 </div>
-                <div className={styles.imageArea}>
-                    {image && <Image src={image} alt="vazantes-representation"/>}
-                    {imageFont && <Link href={imageFont} target="_blank" className={styles.imageFont}>Fonte: {imageFont}</Link>}
-                </div>
+                {image && (
+                    <div className={styles.imageContainer}>
+                        <Image src={image} alt="vazantes-representation"/>
+                        {imageFont && <Link href={imageFont} target="_blank" className={styles.imageFont}>Fonte: {imageFont}</Link>}
+                    </div>
+                )}
             </div>
         </>
     )
