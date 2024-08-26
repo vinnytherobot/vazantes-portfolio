37c5743964920548a4bd28b8703b8853a5bba428
vinnytherobot <dacostamacielf@gmail.com>
2024-08-26 12:01:48 -0300

    Update in MetaTags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,13 @@ import Link from "next/link";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Website created with the aim of displaying points and the historical journey of Vazantes." />
+        <meta name="author" content="vinnytherobot" />
+        <meta property="og:image" content="../../public/img/vazantes-greja.jpg" />
+        <meta property="og:url" content="https://vazantesportfolio.vercel.app/" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <title>Vazantes - Portfólio</title>
       <body>
         <Main />
