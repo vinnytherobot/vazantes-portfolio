9244bf5c72d32a657f8a494aa72341e2e3f0a71b
vinnytherobot <dacostamacielf@gmail.com>
2024-09-02 18:19:33 -0300

    Google ADS added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,9 @@ export default function Document() {
         <meta property="og:url" content="https://vazantesportfolio.vercel.app/" />
         <meta name="google-adsense-account" content="ca-pub-6306379407245797" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6306379407245797" crossOrigin="anonymous"></script>
+
       </Head>
       <title>Vazantes - Portfólio</title>
       <body>
